01b63d74f6e1be0f163b28cc610c3e0e9eea750a
abudi-09 <156515083+abudi-09@users.noreply.github.com>
2025-07-02 21:04:02 +0300

    Update Posts.tsx

--- src/pages/student/Posts.tsx
+++ src/pages/student/Posts.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+ import { useState } from "react";
 import { AppLayout } from "@/components/layout/AppLayout";
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
